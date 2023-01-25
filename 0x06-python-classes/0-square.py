f4552f1c1a3639021ea7078d1d0fe7d37b9c745e
9wilson6 <gatheruwilson@gmail.com>
2023-01-24 23:22:30 -0800

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+'''
+Square Class
+This module contains empty class defining the square
+Square = __import__('0-square').Square
+my_square = Square()
+'''
 class Square:
-    """Blueprint of square"""
+    '''Blueprint of square'''
     pass
