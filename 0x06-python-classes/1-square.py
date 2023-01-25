e4849823169a68f3feb841893b3e311eb5b3dd11
9wilson6 <gatheruwilson@gmail.com>
2023-01-24 23:29:54 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Square class 
+
+Module defines square - has private instance attribute: size
+
+size instantiation - no type
+
+"""
+
+class Square:
+    """defines blueprint of a square - Attribute - size of square object"""
+
+    def __init__(self, size):
+        """ Object constructor method"""
+        self.__size = size
