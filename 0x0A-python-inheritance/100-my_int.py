ab1496a3cb8417cc66a37dc6c0dba992364ade0e
9wilson6 <gatheruwilson@gmail.com>
2023-02-07 00:33:38 -0800

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" module - creating class to inherit from int """
+class MyInt(int):
+    """ class inheriting from int"""
+
+    def __eq__(self, other):
+        """ Equality becomes inequality """
+
+        return super().__ne__(other)
+    def __ne__(self, other):
+        """ inequality becomes equality """
+
+        return super().__eq__(other)
