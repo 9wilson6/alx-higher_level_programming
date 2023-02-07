2b090184421eb825e7f5f096131fad6de6ae4a50
9wilson6 <gatheruwilson@gmail.com>
2023-02-06 22:42:26 -0800

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,6 +2,6 @@
 
 """ Module - finding object is istance of class inherited """
 
-def inherits(obj, a_class):
+def inherits_from(obj, a_class):
     """ determine of obj is instance of class inherited """
     return isinstance(obj, a_class) and type(obj) != a_class
