818d961b0bf4a192103e38f2647e270b37b7e262
9wilson6 <gatheruwilson@gmail.com>
2023-02-07 00:51:42 -0800

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" module - return JSON representation of string """
+
+import json
+def to_json_string(my_obj):
+    """ JSON Representation """
+
+    return json.dumps(my_obj)
