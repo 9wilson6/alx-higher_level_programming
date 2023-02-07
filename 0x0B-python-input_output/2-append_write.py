f30db11f0eaa94d6e5c2ec602cad1cbe37f3caba
9wilson6 <gatheruwilson@gmail.com>
2023-02-07 00:49:26 -0800

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" module - appending string to end of text file """
+
+def append_write(filename="", text=""):
+    """ text to filename """
+    with open(filename, 'a+') as f:
+        return f.write(text)
